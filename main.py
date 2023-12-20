0ac27ce07131c221bb3ed522fb288c4f718b7ca1
CourtSC <scott.c.court@gmail.com>
2023-12-19 21:43:50 -0500

    Created new slash command to play with conditional responses based on user id.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord, os
 from dotenv import load_dotenv
+from discord import app_commands
 
 load_dotenv()
 
@@ -7,20 +8,25 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client=client)
 
 
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=1160743819726299257))
     print(f"We have logged in as {client.user}")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+@tree.command(
+    name="hello",
+    description="A slash command that says 'Hello!'",
+    guild=discord.Object(id=1160743819726299257),
+)
+async def hello(interaction):
+    if str(interaction.user.id) == "329399231229984768":
+        await interaction.response.send_message("Fuck you, Ian.")
+    else:
+        await interaction.response.send_message("Hello, friend.")
 
 
 client.run(os.getenv("TOKEN"))
